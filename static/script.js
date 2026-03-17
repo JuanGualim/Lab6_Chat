dc7af39fe9f8e86275a2e2919a6512f3fd554471
JuanGualim <gua24852@uvg.edu.gt>
2026-03-17 17:58:36 -0600

    Cambio completo de styles.css para mejorar

--- static/script.js
+++ static/script.js
@@ -16,6 +16,9 @@ const getMessages = async () => {
 
     messages.forEach(message => {
         const li = document.createElement("li");
+        if ((message.user || message.author) === "Juan Gualim") {
+            li.classList.add("self");
+        }
         li.innerHTML = `<strong>${message.author || message.user}:</strong> ${message.text}`;
         chatBox.appendChild(li);
     });
@@ -40,15 +43,39 @@ const sendButton = document.getElementById("send-button")
 const userInput = document.getElementById("user-input")
 
 sendButton.addEventListener("click", () => {
-    const message = userInput.value
-    if (message.trim() !== "") {
-        postMessage({
-            user: "Juan Gualim",
-            text: message
-        })
-        userInput.value = ""
+    const message = userInput.value;
+
+    if (message.trim() === "") return;
+
+    if (message.length > 140) {
+        alert("Máximo 140 caracteres");
+        return;
     }
-})
+
+    postMessage({
+        user: "Juan Gualim",
+        text: message
+    });
+
+    userInput.value = "";
+    charCount.textContent = "0 / 140"; // reset
+});
+const charCount = document.getElementById("char-count");
+
+userInput.addEventListener("input", () => {
+    let text = userInput.value;
+
+    if (text.length > 140) {
+        userInput.value = text.substring(0, 140);
+    }
+
+    charCount.textContent = `${userInput.value.length} / 140`;
+});
+if (userInput.value.length >= 120) {
+    charCount.style.color = "red";
+} else {
+    charCount.style.color = "gray";
+}
 userInput.addEventListener("keydown", (e) => {
     if (e.key === "Enter") {
         e.preventDefault(); // 👈 evita salto de línea
